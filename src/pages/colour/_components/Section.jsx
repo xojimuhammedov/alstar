b64f8dcb70833ee6c30e23aef4df4c9470fd519a
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-10-08 14:22:58 +0500

    add

--- src/pages/colour/_components/Section.jsx
+++ src/pages/colour/_components/Section.jsx
@@ -1,221 +1,44 @@
-import { Box, Heading, Image, SimpleGrid } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Heading, Image, SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
 
-import ScreenSix from "@/assets/screen10.jpg";
-import ScreenSeven from "@/assets/screen11.jpg";
+import axios from 'axios';
+import { BASE_URL, FILE_URL } from '@/api';
+
+import { useTranslation } from 'react-i18next';
+import AboutColor from './AboutColor';
 
 function Section() {
+  const { t, i18n } = useTranslation();
+  const [color, setColor] = useState();
+  const [colorId, setColorId] = useState('')
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/colours?limit=1000`)
+      .then((res) => {
+        setColor(res?.data?.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Box p={'24px 0'}>
       <Box className="container-mix">
         <SimpleGrid gap={'24px'} columns={4}>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/06/846-RAL-6018_600x600_Yellow_Green.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Yellow Green | MA6018-G30 </Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/06/66-M7815-G30_Adobe_Pink.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Adobe Pink | M7815-G30</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/01/911-Alu_Champagne_Brushed_ML_0013-G40-ret.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Alu Champagne Brushed | ML0013-G40</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/03/53-M0556-G30_Aluminium-Gray.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Aluminium Gray | M0556-G30</Heading>
-          </Box>
-
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/08/868-Amethyst_Purple_MI_0020-G30-ret.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Amethyst Purple | MI0020-G30</Heading>
-          </Box>
-
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/02/912-Anodic_Brushed_ML_0003-G10-ret.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Anodic Brushed | ML0003-G10</Heading>
-          </Box>
-
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/02/832-Anodic-Silver_DE-MD9500-G30_Ret_2.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Anodic Silver | MD9500-G30</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/00/810-Anodisation_Copper_ME_0049.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Anodisation Copper | 049</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/01/811-Anodisation_Red_058.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Anodisation Red | 058</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/02/812-Anodized_Silver_044.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Anodisation Silver | 044</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/03/833-Anthracite_Gray_MA_7105-G30.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Anthracite Gray | MA7105-G30</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/06/76-MB126-G30_Apple_Red.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Apple Red |MB126-G30</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/09/79-MBS02-G30_Aqua-Green.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Aqua Green | MBS02-G30</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/09/869-Arctic_Gold_MI_0021-G30-ret.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Arctic Gold | MI0021-G30</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/07/867-Artic_Gray_MI_0019-G80-ret.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Artic Gray | MI0019-G80</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/07/107-MJ062-Black.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Black | MJ062-G30</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/03/813-Black_C35_045.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Black C35 | 045</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/04/834-Black_Gray_MA_7021-G30.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Black Gray | MA7021-G30</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/06/866-Black_Titanium_MI_0022-G80-ret.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Black Titanium | MI0022-G80</Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/02/62-M7787-G30_Blue.jpg?v=1-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Blue | M7787-G30</Heading>
-          </Box>
+          {color?.map((item, index) => (
+            <Box key={index}>
+              <Image onClick={() => {
+                onOpen(),
+                setColorId(item?.id)
+              }} {...css.image} src={`${FILE_URL}/files/${item?.image}`} alt="DesignImage" />
+              <Heading {...css.name}>{item[`name_${i18n?.language}`]}</Heading>
+            </Box>
+          ))}
         </SimpleGrid>
-        <Image src={ScreenSix.src} alt="Image" />
-        <Image src={ScreenSeven.src} alt="Image" />
       </Box>
+
+      <AboutColor colorId={colorId} isOpen={isOpen} onClose={onClose} />
     </Box>
   );
 }
@@ -227,7 +50,8 @@ const css = {
     maxWidth: '100%',
     minWidth: '100%',
     width: '100%',
-    height: '120px'
+    height: '120px',
+    cursor: 'pointer'
   },
   name: {
     fontSize: '16px',
