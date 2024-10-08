cb2d6a3d67b447be4dd2afe93aacee9379b8f15c
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-10-08 17:03:03 +0500

    update

--- src/pages/colour/_components/Section.jsx
+++ src/pages/colour/_components/Section.jsx
@@ -1,17 +1,23 @@
-import { Box, Heading, Image, SimpleGrid, useDisclosure } from '@chakra-ui/react';
-import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Heading,
+  Image,
+  SimpleGrid,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
 
-import axios from 'axios';
-import { BASE_URL, FILE_URL } from '@/api';
+import axios from "axios";
+import { BASE_URL, FILE_URL } from "@/api";
 
-import { useTranslation } from 'react-i18next';
-import AboutColor from './AboutColor';
+import { useTranslation } from "react-i18next";
+import AboutColor from "./AboutColor";
 
 function Section() {
   const { t, i18n } = useTranslation();
   const [color, setColor] = useState();
-  const [colorId, setColorId] = useState('')
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [colorId, setColorId] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     axios
@@ -23,15 +29,19 @@ function Section() {
   }, []);
 
   return (
-    <Box p={'24px 0'}>
+    <Box p={"24px 0"}>
       <Box className="container-mix">
-        <SimpleGrid gap={'24px'} columns={4}>
+        <SimpleGrid gap={"24px"} columns={4}>
           {color?.map((item, index) => (
             <Box key={index}>
-              <Image onClick={() => {
-                onOpen(),
-                setColorId(item?.id)
-              }} {...css.image} src={`${FILE_URL}/files/${item?.image}`} alt="DesignImage" />
+              <Image
+                onClick={() => {
+                  onOpen(), setColorId(item?.id);
+                }}
+                {...css.image}
+                src={`${FILE_URL}/files/${item?.image}`}
+                alt="DesignImage"
+              />
               <Heading {...css.name}>{item[`name_${i18n?.language}`]}</Heading>
             </Box>
           ))}
@@ -47,17 +57,17 @@ export default Section;
 
 const css = {
   image: {
-    maxWidth: '100%',
-    minWidth: '100%',
-    width: '100%',
-    height: '120px',
-    cursor: 'pointer'
+    maxWidth: "100%",
+    minWidth: "100%",
+    width: "100%",
+    height: "120px",
+    cursor: "pointer",
   },
   name: {
-    fontSize: '16px',
-    lineHeight: '25px',
-    color: '#111',
-    marginTop: '12px',
-    fontWeight: '400'
-  }
+    fontSize: "16px",
+    lineHeight: "25px",
+    color: "#111",
+    marginTop: "12px",
+    fontWeight: "400",
+  },
 };
--- src/pages/projects-list/_components/Section.jsx
+++ src/pages/projects-list/_components/Section.jsx
@@ -1,112 +1,33 @@
 import { Box, Heading, Image, SimpleGrid } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BASE_URL, FILE_URL } from '@/api';
 
-import ScreenOne from "@/assets/screen.jpg";
-import ScreenTwo from "@/assets/screen1.jpg";
-import ScreenThree from "@/assets/screen2.jpg";
+import { useTranslation } from 'react-i18next';
 
 function Section() {
+  const { t, i18n } = useTranslation();
+  const [project, setProject] = useState();
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/news?limit=1000`)
+      .then((res) => {
+        setProject(res?.data?.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Box p={'24px 0'}>
       <Box className="container-mix">
-        <SimpleGrid gap={'24px'} columns={4}>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/05/355-108_n_state_street.webp?v=2-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>108N. State Street
-            </Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/06/356-A.T.-Kearney_Ljubljana_Slovenia.webp?v=2-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>A.T. Kearney
-            </Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/07/167-Kachel_fr_Abdoun-Shopping-Atrium_750x400.webp?v=3-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Abdoun Shopping Atrium
-            </Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/08/358-Abeno-Harukas_Japan.webp?v=2-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Abeno Harukas
-            </Heading>
-          </Box>
-
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/09/169-Kachel_fr_Aldar-Headquater_Abu-Dhabi_UAE_01_750x400.webp?v=2-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Aldar Headquarter
-            </Heading>
-          </Box>
-
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/00/170-Kachel_fr_ALTARA-Shopping-Mall_Honduras_750x400.webp?v=2-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>ALTARA Shopping Mall
-            </Heading>
-          </Box>
-
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/01/171-Kachel_fr_Ansan-Wongok-Bondong-Resident-Center_750x400.webp?v=2-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Ansan Wongok Bondong Resident Center
-            </Heading>
-          </Box>
-          <Box>
-            <Image
-              {...css.image}
-              src={
-                'https://www.alpolic.eu/uploads/media/fullsize-3840x/09/359-Assembly-Hall_Vietnam.webp?v=2-0'
-              }
-              alt="DesignImage"
-            />
-            <Heading {...css.name}>Assembly Hall
-            </Heading>
-          </Box>
-       
-              
+        <SimpleGrid gap={'24px'} columns={3}>
+          {project?.map((item, index) => (
+            <Box key={index}>
+              <Image {...css.image} src={`${FILE_URL}/files/${item?.image}`} alt="DesignImage" />
+              <Heading {...css.name}>{item[`name_${i18n?.language}`]}</Heading>
+            </Box>
+          ))}
         </SimpleGrid>
-        <Image src={ScreenOne.src} alt="Image" />
-        <Image src={ScreenTwo.src} alt="Image" />
-        <Image src={ScreenThree.src} alt="Image" />
       </Box>
     </Box>
   );
@@ -119,7 +40,7 @@ const css = {
     maxWidth: '100%',
     minWidth: '100%',
     width: '100%',
-    height: '120px'
+    height: '180px'
   },
   name: {
     fontSize: '16px',
