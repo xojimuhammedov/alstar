5aefea902a86244a0b719a61df2de485b012612a
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-10-11 16:11:48 +0500

    add

--- src/pages/projects-hight/_components/Header.jsx
+++ src/pages/projects-hight/_components/Header.jsx
@@ -1,16 +1,11 @@
 import { Box, Image } from '@chakra-ui/react';
 import React from 'react';
+import ImageFour from '@/assets/image2.jpg';
 
 function Header() {
   return (
     <Box position={'relative'}>
-      <Image
-        {...css.image}
-        src={
-          'https://www.alpolic.eu/uploads/media/fullsize-1920x/07/7-header_webnews_3840x537_neu_4.webp?v=6-0'
-        }
-        alt="BannerOne"
-      />
+      <Image {...css.image} src={ImageFour.src} alt="BannerOne" />
     </Box>
   );
 }
@@ -22,6 +17,7 @@ const css = {
     width: '100%',
     maxWidth: '100%',
     minWidth: '100%',
-    height: '200px'
+    height: '200px',
+    objectFit:"cover"
   }
 };
--- src/pages/projects-hight/_components/Main.jsx
+++ src/pages/projects-hight/_components/Main.jsx
@@ -1,28 +1,15 @@
-import { Box, Heading, SimpleGrid, Text, Image, Link } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-
-import ScreenOne from "@/assets/screen4.jpg";
-import ScreenTwo from "@/assets/screen5.jpg";
-import ScreenThree from "@/assets/screen6.jpg";
-import ScreenFour from "@/assets/screen3.jpg";
-import ScreenFive from "@/assets/screen7.jpg";
-import ScreenSix from "@/assets/screen8.jpg";
 
 function Main() {
   const { t } = useTranslation();
   return (
     <Box p={'36px 0'}>
       <Box className="container-mix">
-        <Heading {...css.title}>{t('BE.UP.TO.DATE.')}</Heading>
-        <Text {...css.text}>{t('newsListText')}</Text>
-        <Image src={ScreenFour.src} alt="Image" />
-        <Image src={ScreenOne.src} alt="Image" />
-        <Image src={ScreenTwo.src} alt="Image" />
-        <Image src={ScreenThree.src} alt="Image" />
-        <Image src={ScreenFive.src} alt="Image" />
-        <Image src={ScreenSix.src} alt="Image" />
+        <Heading {...css.title}>{t('inspirations.name')}</Heading>
+        <Text {...css.text}>{t('inspirations.text')}</Text>
       </Box>
     </Box>
   );
